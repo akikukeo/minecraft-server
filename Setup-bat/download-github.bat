d0f15775d9524077d39b0a28329ab5cf13235fef
akikukeo <165561768+akikukeo@users.noreply.github.com>
2024-09-22 21:34:32 +0900

    Update download-github.bat

--- Setup-bat/download-github.bat
+++ Setup-bat/download-github.bat
@@ -1,11 +1,6 @@
 @echo off
 setlocal
 
-::  GitHubリポジトリの情報を設定
-set "GITHUB_URL=https://github.com/akikukeo/minecraft-server/archive/refs/heads/main.zip"
-set "ZIP_FILE=.\minecraft-server.zip"
-set "EXTRACT_DIR=.\minecraft-server-main"
-
 ::  カレントディレクトリを表示
 echo Current directory: %CD%
 
