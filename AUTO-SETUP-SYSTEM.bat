09c199a1bf317275f341d302f7cd987f141b83f6
akikukeo <165561768+akikukeo@users.noreply.github.com>
2024-09-22 21:37:58 +0900

    update

--- AUTO-SETUP-SYSTEM.bat
+++ AUTO-SETUP-SYSTEM.bat
@@ -1,3 +1,7 @@
 @echo off
 setlocal
+cd Setup-bat
 
+call set.bat
+:: testecho
+pause
--- Setup-bat/set.bat
+++ Setup-bat/set.bat
@@ -1,3 +1,6 @@
+@echo off
+setlocal
+
 ::  GitHubリポジトリの情報を設定
 set "GITHUB_URL=https://github.com/akikukeo/minecraft-server/archive/refs/heads/main.zip"
 set "ZIP_FILE=.\minecraft-server.zip"
