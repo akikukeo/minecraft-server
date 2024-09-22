5d0cc1a727a06b00f5cdefba2e9ea66b5ddd23ed
akikukeo <165561768+akikukeo@users.noreply.github.com>
2024-09-23 08:28:17 +0900

    update

--- Auto-startup.bat
+++ Auto-startup.bat
@@ -1,6 +1,7 @@
 @echo off
 title Auto-startup.bat
 setlocal enabledelayedexpansion
+chcp 932
 
 cd ./Startup-bat
 :: config 取得
@@ -8,10 +9,8 @@ for /f "tokens=1,2 delims==" %%a in (config.ini) do (
  set %%a=%%b
 )
 
-echo %cd%
-echo %java_Xms_M%
 set java_Xms_M_re=-Xms%java_Xms_M%M -Xmx%java_Xms_M%M -jar paper-1.21.1-85.jar
 
 call Paper-run.bat
-
-exit /b
+endlocal
+exit
