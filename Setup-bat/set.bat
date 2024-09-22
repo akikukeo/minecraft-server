9943737dc201994c9a2e61e97be29b309f46ab4c
akikukeo <165561768+akikukeo@users.noreply.github.com>
2024-09-22 21:15:25 +0900

    Create set.bat

---
+++ Setup-bat/set.bat
@@ -0,0 +1,4 @@
+::  GitHubリポジトリの情報を設定
+set "GITHUB_URL=https://github.com/akikukeo/minecraft-server/archive/refs/heads/main.zip"
+set "ZIP_FILE=.\minecraft-server.zip"
+set "EXTRACT_DIR=.\minecraft-server-main"
