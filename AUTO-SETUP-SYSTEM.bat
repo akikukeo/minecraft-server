7fd79324f4f09d12b5f6c0dc8eef11370d76ceeb
akikukeo <165561768+akikukeo@users.noreply.github.com>
2024-09-23 10:16:26 +0900

    up

--- AUTO-SETUP-SYSTEM.bat
+++ AUTO-SETUP-SYSTEM.bat
@@ -1,28 +1,44 @@
 @echo off
 chcp 932
 setlocal enabledelayedexpansion
-set title=自動セットアップシステム実行中...
-title %title%
+title 自動ダウンロードシステム実行中...
 
-@REM config.ini を読み込む
-    cd Setup-bat
-    @REM config.ini を読み込む
-    for /f "tokens=1,2 delims==" %%a in (config.ini) do (
-        set %%a=%%b
+set GITHUB_URL=https://github.com/akikukeo/minecraft-server/archive/refs/heads/inst-sys.zip
+set ZIP_FILE=.\minecraft-server-inst-sys.zip
+set EXTRACT_DIR=.\minecraft-inst-sys
+
+
+
+    md install
+    cd install
+    
+@REM curlを使ってリポジトリをダウンロード
+    echo Downloading repository...
+    curl -L -o %ZIP_FILE% %GITHUB_URL%
+
+@REM ダウンロードが成功したかを確認
+    if not exist %ZIP_FILE% (
+        echo %ERROR_R% ダウンロードに失敗しました。処理を終了します。
+        exit /b 1
+    )  else (
+            echo %LOG_R% ダウンロードが完了しました。
     )
 
+@REM tarを使ってZIPファイルを解凍
+    echo %LOG_R% ZIPを展開しています...
+    tar -xf %ZIP_FILE%
 
-
-@REM ダウンロードする
-    call download-github.bat
-    if errorlevel 1 (
-        title %title%
-        echo !ERROR_R! 自動ダウンロードシステムの失敗を検知しました。処理を終了します。
-    )
-    if errorlevel 0 (
-        title %title%
-        echo !SUCCESS_R! 自動ダウンロードシステムの成功を検知しました。処理を終了します。
+@REM 解凍が成功したかを確認
+    if not exist %EXTRACT_DIR% (
+        echo %LOG_R% 展開に失敗しました。処理を終了します。
+        exit /b 1
     )
 
+@REM ダウンロードしたZIPファイルを削除
+    echo %LOG_R% ZIPを削除しています...
+    del %ZIP_FILE%
+
+echo %LOG_R% すべてのダウンロードが完了しました。
 endlocal
 pause
+exit /b 0
