04e45d5939a1cd5e996a3e6c1d8bdd90b756bf61
akikukeo <165561768+akikukeo@users.noreply.github.com>
2024-09-23 16:33:23 +0900

    upd

--- Auto-startup.bat
+++ Auto-startup.bat
@@ -3,14 +3,18 @@ title Auto-startup.bat
 chcp 932
 setlocal enabledelayedexpansion
 
+@REM proxサーバー起動
+    cd Servers/Prox-velo
+    start run.bat
+
 cd ./Startup-bat
-:: config ﾅｽﾃｦ窶慊ｾ
-for /f "tokens=1,2 delims==" %%a in (config.ini) do (
- set %%a=%%b
-)
+@REM config読み
+    for /f "tokens=1,2 delims==" %%a in (config.ini) do (
+    set %%a=%%b
+    )
 
 set java_Xms_M_re=-Xms%java_Xms_M%M -Xmx%java_Xms_M%M -jar paper-1.21.1-85.jar
+start Paper-run.bat
 
-call Paper-run.bat
 endlocal
 exit
