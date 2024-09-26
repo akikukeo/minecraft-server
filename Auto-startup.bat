a3f9aec6ac9c769046596e46af379bc338c63fa4
akikukeo <165561768+akikukeo@users.noreply.github.com>
2024-09-26 16:34:44 +0900

    configシステムの構築

--- Auto-startup.bat
+++ Auto-startup.bat
@@ -1,17 +1,35 @@
 @echo off
 title Auto-startup.bat
-chcp 65001
+@chcp 65001
 setlocal enabledelayedexpansion
 
-@REM config読み
-    echo コンフィグを読み込んでいます...
-    for /f "tokens=1,2 delims==" %%a in (config.ini) do (
-    set %%a=%%b
-    )
-    echo 読み込みが完了しました。
+@REM 設定する
 
-@REM set java_Xms_M_re=-Xms%java_Xms_M%M -Xmx%java_Xms_M%M -jar paper-1.21.1-85.jar
-@REM start Paper-run.bat
+    @REM config読みこみ
+        for /f "tokens=1,2 delims==" %%a in (config.ini) do (
+        set %%a=%%b
+        )
+
+    @REM タイムスタンプの設定
+        for /f "tokens=2 delims==" %%a in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%a
+        set time=!datetime:~8,6!   :: HHMMSS
+
+    @REM タイムスタンプを[時:分:秒]形式に変更
+        set hour=!time:~0,2!
+        set minute=!time:~2,2!
+        set second=!time:~4,2!
+        set timestamp=[!hour!:!minute!:!second!]
+    echo !timestamp!!SUC! 設定読み込みが完了しました。
+
+
+@REM java引数の設定
+
+    @REM configから読み込む
+        echo !timestamp!!LOG! java引数を読み込みます...
+        set java_Xm_xs_M=-Xms!java_Xms_M!M -Xmx!java_Xmx_M!M -jar paper-1.21.1-85.jar
+        echo !timestamp!!SUC! java引数の設定が完了しました。
+pause
+
 
 endlocal
 exit
