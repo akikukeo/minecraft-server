23dd338a3474cf7ea0b8094ad2d3537db4e5a536
akikukeo <165561768+akikukeo@users.noreply.github.com>
2024-10-03 11:35:38 +0900

    edit venv  add flask

--- Web/test.py
+++ Web/test.py
@@ -1,14 +1,9 @@
-from flask import Flask, jsonify, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/api/data')
-def get_data():
-    return jsonify({'message': 'Hello from the server!'})
+def hello():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
